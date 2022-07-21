3abf7ffde8c11b980601a5f7983f85e143cfd217
PuruPanda1 <purabmodi2002@gmail.com>
2022-07-21 13:29:16 +0530

    Fixed minor bugs
    App was crashing when data was not present due to viewmodel's null value

--- app/src/main/java/com/example/payment/fragments/mainScreen/MainScreen.kt
+++ app/src/main/java/com/example/payment/fragments/mainScreen/MainScreen.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
@@ -55,8 +56,14 @@ class MainScreen : Fragment() {
 
 //        set balance amount
         viewModel.readDifferenceSum.observe(viewLifecycleOwner, Observer {
-            binding.balanceAmount.text =
-                String.format(getString(R.string.amountInRupee, it.toString()))
+            if(it!=null) {
+                binding.balanceAmount.text =
+                    String.format(getString(R.string.amountInRupee, it.toString()))
+            }
+            else{
+                binding.balanceAmount.text =
+                    String.format(getString(R.string.amountInRupee, "0"))
+            }
         })
 
 //        set monthly balance amount
@@ -64,8 +71,14 @@ class MainScreen : Fragment() {
         binding.monthlyDuration.text = String.format(getString(R.string.monthlyDuration, month))
 //        observer for balance
         viewModel.readMonthlySpends.observe(viewLifecycleOwner, Observer {
-            binding.monthlyExpense.text =
-                String.format(getString(R.string.amountInRupee, it.toString()))
+            if(it!=null) {
+                binding.monthlyExpense.text =
+                    String.format(getString(R.string.amountInRupee, it.toString()))
+            }
+            else{
+                binding.monthlyExpense.text =
+                    String.format(getString(R.string.amountInRupee, "0"))
+            }
         })
 
 //         setting the pie chart
@@ -74,12 +87,16 @@ class MainScreen : Fragment() {
         var income = 0f
         var expense = 0f
         viewModel.incomeSum.observe(viewLifecycleOwner, Observer {
-            income = it
+            if (it != null) {
+                income = it
+            }
             updateChart(income, expense)
         })
 
         viewModel.expenseSum.observe(viewLifecycleOwner, Observer {
-            expense = it
+            if (it != null) {
+                expense = it
+            }
             updateChart(income, expense)
         })
 
@@ -90,90 +107,95 @@ class MainScreen : Fragment() {
 
 //      setting the data for the recent transactions
         viewModel.readAllTransaction.observe(viewLifecycleOwner, Observer { transactionList ->
-//            setting amount for the 1st transaction
-            if (transactionList[0].isExpense) {
-                binding.amountShowMainScreen1.text =
-                    transactionList[0].expenseAmount.toString()
-                binding.amountShowMainScreen1.setTextColor(
-                    ContextCompat.getColor(
-                        requireContext(),
-                        R.color.expense_color
-                    )
-                )
+            if (transactionList.isEmpty() || transactionList.size < 3) {
+                binding.eachRowLayout1.isVisible = false
+                binding.eachRowLayout2.isVisible = false
+                binding.eachRowLayout3.isVisible = false
             } else {
-                binding.amountShowMainScreen1.text =
-                    transactionList[0].incomeAmount.toString()
-                binding.amountShowMainScreen1.setTextColor(
-                    ContextCompat.getColor(
-                        requireContext(),
-                        R.color.income_color
+                //            setting amount for the 1st transaction
+                if (transactionList[0].isExpense) {
+                    binding.amountShowMainScreen1.text =
+                        transactionList[0].expenseAmount.toString()
+                    binding.amountShowMainScreen1.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.expense_color
+                        )
                     )
-                )
-            }
+                } else {
+                    binding.amountShowMainScreen1.text =
+                        transactionList[0].incomeAmount.toString()
+                    binding.amountShowMainScreen1.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.income_color
+                        )
+                    )
+                }
 //            setting amount for the 2nd transaction
-            if (transactionList[1].isExpense) {
-                binding.amountShowMainScreen2.text =
-                    transactionList[1].expenseAmount.toString()
-                binding.amountShowMainScreen2.setTextColor(
-                    ContextCompat.getColor(
-                        requireContext(),
-                        R.color.expense_color
+                if (transactionList[1].isExpense) {
+                    binding.amountShowMainScreen2.text =
+                        transactionList[1].expenseAmount.toString()
+                    binding.amountShowMainScreen2.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.expense_color
+                        )
                     )
-                )
-            } else {
-                binding.amountShowMainScreen2.text =
-                    transactionList[1].incomeAmount.toString()
-                binding.amountShowMainScreen2.setTextColor(
-                    ContextCompat.getColor(
-                        requireContext(),
-                        R.color.income_color
+                } else {
+                    binding.amountShowMainScreen2.text =
+                        transactionList[1].incomeAmount.toString()
+                    binding.amountShowMainScreen2.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.income_color
+                        )
                     )
-                )
-            }
+                }
 //            setting amount for the 3rd transaction
-            if (transactionList[2].isExpense) {
-                binding.amountShowMainScreen3.text =
-                    transactionList[2].expenseAmount.toString()
-                binding.amountShowMainScreen3.setTextColor(
-                    ContextCompat.getColor(
-                        requireContext(),
-                        R.color.expense_color
+                if (transactionList[2].isExpense) {
+                    binding.amountShowMainScreen3.text =
+                        transactionList[2].expenseAmount.toString()
+                    binding.amountShowMainScreen3.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.expense_color
+                        )
                     )
-                )
-            } else {
-                binding.amountShowMainScreen3.text =
-                    transactionList[2].incomeAmount.toString()
-                binding.amountShowMainScreen3.setTextColor(
-                    ContextCompat.getColor(
-                        requireContext(),
-                        R.color.income_color
+                } else {
+                    binding.amountShowMainScreen3.text =
+                        transactionList[2].incomeAmount.toString()
+                    binding.amountShowMainScreen3.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.income_color
+                        )
                     )
-                )
-            }
+                }
 //            setting date
-            val simpleDateFormat = SimpleDateFormat("dd MMM")
-            var dateString = simpleDateFormat.format(transactionList[0].date)
-            binding.dateShowMainScreen1.text = dateString
-            dateString = simpleDateFormat.format(transactionList[1].date)
-            binding.dateShowMainScreen2.text = dateString
-            dateString = simpleDateFormat.format(transactionList[2].date)
-            binding.dateShowMainScreen3.text = dateString
+                val simpleDateFormat = SimpleDateFormat("dd MMM")
+                var dateString = simpleDateFormat.format(transactionList[0].date)
+                binding.dateShowMainScreen1.text = dateString
+                dateString = simpleDateFormat.format(transactionList[1].date)
+                binding.dateShowMainScreen2.text = dateString
+                dateString = simpleDateFormat.format(transactionList[2].date)
+                binding.dateShowMainScreen3.text = dateString
 //            setting category values
-            binding.categoryShowMainScreen1.text =
-                transactionList[0].transactionType
-            binding.categoryShowMainScreen2.text =
-                transactionList[1].transactionType
-            binding.categoryShowMainScreen3.text =
-                transactionList[2].transactionType
+                binding.categoryShowMainScreen1.text =
+                    transactionList[0].transactionType
+                binding.categoryShowMainScreen2.text =
+                    transactionList[1].transactionType
+                binding.categoryShowMainScreen3.text =
+                    transactionList[2].transactionType
 //            setting description
-            binding.descriptionShowMainScreen1.text =
-                transactionList[0].tDescription
-            binding.descriptionShowMainScreen2.text =
-                transactionList[1].tDescription
-            binding.descriptionShowMainScreen3.text =
-                transactionList[2].tDescription
+                binding.descriptionShowMainScreen1.text =
+                    transactionList[0].tDescription
+                binding.descriptionShowMainScreen2.text =
+                    transactionList[1].tDescription
+                binding.descriptionShowMainScreen3.text =
+                    transactionList[2].tDescription
+            }
         })
-
         return binding.root
     }
 
