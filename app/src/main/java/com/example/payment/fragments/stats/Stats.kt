00527cf75713306cc7eec26deab36fec0aa878b4
PuruPanda1 <purabmodi2002@gmail.com>
2022-07-21 11:41:33 +0530

    Added toast in delete option in Stats Fragment

--- app/src/main/java/com/example/payment/fragments/stats/Stats.kt
+++ app/src/main/java/com/example/payment/fragments/stats/Stats.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
@@ -77,6 +78,7 @@ class Stats : Fragment() {
 
     fun deleteTransaction(transaction: Transaction) {
         viewModel.deleteTransaction(transaction)
+        Toast.makeText(requireContext(), "Deleted", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroyView() {
